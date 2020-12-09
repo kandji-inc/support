b7d07f844afd1e3759ba97468f63c0d2306c77e2
Nick McDonald <59895032+nick-mcdonald-dtnb@users.noreply.github.com>
2020-12-08 18:47:19 -0800

    Update ResetSudoersDefaultTimeout.sh
    Added additonal validation

--- Scripts/ResetSudoersDefaultTimeout.sh
+++ Scripts/ResetSudoersDefaultTimeout.sh
@@ -31,20 +31,56 @@
 ################################################################################################
 
 #Copies the current sudoers file
-/bin/cp /etc/sudoers /tmp/sudoers
+/bin/cp /etc/sudoers /tmp/sudoers-tmp
 
 #Uses sed to find and replace the default timestamp line
-/usr/bin/sed -i "" 's/Defaults timestamp\_timeout\=0/Defaults timestamp\_timeout\=5/g' /tmp/sudoers
+/usr/bin/sed -i -backup 's/Defaults timestamp\_timeout\=0/Defaults timestamp\_timeout\=5/g' /tmp/sudoers-tmp
 
 #Uses visudo to validate the syntax of the new file prior to copying
-/usr/sbin/visudo -cf /tmp/sudoers
+/usr/sbin/visudo -cf /tmp/sudoers-tmp
+
+attempt="1"
+
+fCopyModifiedFile ()
+{
+	while [ ${attempt} -lt 4 ]
+	do
+		
+		if [ ${attempt} -gt 1 ]; then 
+			sleep 10
+		fi
+	
+			echo "Starting copy attempt number ${attempt}"
+			/bin/cp /tmp/sudoers-tmp /etc/sudoers
+			
+			if [ $? -eq 0 ]; then
+				echo "Sudoers file replaced successfully."
+				fDeleteTmpFiles
+				exit 0
+			else
+				echo "Failed to copy the new file ${attempt} times... Attempting recovery..."
+				attempt=$[${attempt}+1]
+				fCopyModifiedFile
+			fi
+	done
+	
+	echo "Max copy attempts reached. Please restore the backup manually from /tmp/sudoers-backup"
+	exit 2
+}
+
+fDeleteTmpFiles ()
+{
+	echo "Deleting /tmp/sudoers-backup-modified & /tmp/sudoers-backup"
+	/bin/rm -f "/tmp/sudoers-tmp-backup"
+	/bin/rm -f "/tmp/sudoers-tmp"
+}
 
 #Checks if the validation was successful; if so, proceeds with copying the new file.
 if [ $? -eq 0 ]; then
-	echo "Sudoers file validated"
-	/bin/cp /tmp/sudoers /etc/sudoers
-	exit $?
+	echo "Sudoers file validated... attempting copy"
+	fCopyModifiedFile
 else
-	echo "Syntax validation failed. Please edit the file manually using visudo"
+	echo "Syntax validation failed. Please edit the file manually using visudo... removing backup and modified files since nothing was changed..."
+	fDeleteTmpFiles
 	exit 1
 fi
