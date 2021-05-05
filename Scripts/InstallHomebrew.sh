60e79eb8a838c21a32f809c10638992e64f10237
Nick McDonald <59895032+nick-mcdonald-dtnb@users.noreply.github.com>
2021-05-04 17:46:29 -0700

    Update InstallHomebrew.sh
    Updated and added rosetta support

--- Scripts/InstallHomebrew.sh
+++ Scripts/InstallHomebrew.sh
@@ -3,7 +3,7 @@
 ######################################################################################################
 # Created by Nicholas McDonald | se@kandji.io | Kandji, Inc. | Solutions Engineering
 ######################################################################################################
-# Created on 08/10/2020
+# Created on 08/10/2020 updated on 05/04/2021
 ######################################################################################################
 # Software Information
 ######################################################################################################
@@ -40,7 +40,7 @@
 #
 ################################################################################################
 
-mostCommonUser=$(/usr/bin/last -t console | awk '!/_mbsetupuser|root|wtmp/' | /usr/bin/cut -d" " -f1 | /usr/bin/uniq -c | /usr/bin/sort -nr | /usr/bin/head -n1 | /usr/bin/grep -o '[a-zA-Z].*')
+mostCommonUser=$(/usr/bin/last -t console | /usr/bin/awk '!/_mbsetupuser|root|wtmp/' | /usr/bin/cut -d" " -f1 | /usr/bin/uniq -c | /usr/bin/sort -nr | /usr/bin/head -n1 | /usr/bin/grep -o '[a-zA-Z].*')
 
 if [ -e "/usr/local/bin/brew" ]; then 
 	echo "Brew is already installed..."
@@ -56,49 +56,92 @@ fi
 
 echo "${mostCommonUser} is the most common console user... installing homebrew as this user"
 
-export HOME=$(/usr/bin/mktemp -d)
+# Set environment variables
+HOME="$(mktemp -d)"
+export HOME
 export USER=root
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-BREW_INSTALL_LOG=$(/usr/bin/mktemp)
+BREW_INSTALL_LOG=$(mktemp)
 
-# Install Homebrew | removes all interactive prompts
-/bin/bash -c "$(/usr/bin/curl -fsSL \
+# Verify the TargetUser is valid
+if /usr/bin/dscl . -read "/Users/${mostCommonUser}" 2>&1 >/dev/null; then
+	/bin/echo "Validated ${mostCommonUser}"
+else
+	/bin/echo "Specified user \"${mostCommonUser}\" is invalid"
+	exit 1
+fi
+
+#Determine the processor brand
+processorBrand=$(/usr/sbin/sysctl -n machdep.cpu.brand_string)
+
+if [[ "${processorBrand}" = *"Apple"* ]]; then
+	echo "Apple Processor is present..."
+	#Check if the Rosetta service is running
+	checkRosettaStatus=$(/bin/launchctl list | /usr/bin/grep "com.apple.oahd-root-helper")
+	
+	if [[ "${checkRosettaStatus}" != "" ]]; then
+		echo "Rosetta is installed... no action needed"
+	else
+		echo "Rosetta is not installed... installing now"
+		#Installs Rosetta
+		/usr/sbin/softwareupdate --install-rosetta --agree-to-license
+		
+		#Checks the outcome of the Rosetta install
+		if [[ $? -eq 0 ]]; then
+			echo "Rosetta installed..."
+		else
+			echo "Rosetta install failed..."
+			exit 1
+		fi
+	fi
+else
+	echo "Apple Processor is not present... rosetta not needed"
+fi
+
+
+# Install Homebrew | strip out all interactive prompts
+/bin/bash -c "$(curl -fsSL \
 	https://raw.githubusercontent.com/Homebrew/install/master/install.sh | \
 	sed "s/abort \"Don't run this as root\!\"/\
 	echo \"WARNING: Running as root...\"/" | \
-	sed 's/  wait_for_user/  :/')" 2>&1 | /usr/bin/tee ${BREW_INSTALL_LOG}
+	sed 's/  wait_for_user/  :/')" 2>&1 | tee "${BREW_INSTALL_LOG}"
 
 # Reset Homebrew permissions for target user
 brew_file_paths=$(/usr/bin/sed '1,/==> This script will install:/d;/==> /,$d' \
-	${BREW_INSTALL_LOG})
-	
+	"${BREW_INSTALL_LOG}")
 brew_dir_paths=$(/usr/bin/sed '1,/==> The following new directories/d;/==> /,$d' \
-	${BREW_INSTALL_LOG})
-	
+	"${BREW_INSTALL_LOG}")
+# Get the paths for the installed brew binary
+brew_bin=$(echo "${brew_file_paths}" | grep "/bin/brew")
+brew_bin_path=${brew_bin%/brew}
+# shellcheck disable=SC2086
 /usr/sbin/chown -R "${mostCommonUser}":admin ${brew_file_paths} ${brew_dir_paths}
-
-/usr/bin/chgrp admin /usr/local/bin/
-
-/bin/chmod g+w /usr/local/bin
+/usr/bin/chgrp admin ${brew_bin_path}/
+/bin/chmod g+w ${brew_bin_path}
 
 # Unset home/user environment variables
 unset HOME
 unset USER
 
-# Finish brew installation by forcing an update 
-/usr/bin/su - "${mostCommonUser}" -c "/usr/local/bin/brew update --force"
+# Finish up Homebrew install as target user
+/usr/bin/su - "${mostCommonUser}" -c "${brew_bin} update --force"
 
-# Run brew cleanup prior to checking install status
-/usr/bin/su - "${mostCommonUser}" -c "/usr/local/bin/brew cleanup"
+# Run cleanup before checking in with the doctor
+/usr/bin/su - "${mostCommonUser}" -c "${brew_bin} cleanup"
 
-# Check brew install status
-/usr/bin/su - "${mostCommonUser}" -c "/usr/local/bin/brew doctor"
+# Check for missing PATH
+get_path_cmd=$(/usr/bin/su - "${mostCommonUser}" -c "${brew_bin} doctor 2>&1 | /usr/bin/grep 'export PATH=' | /usr/bin/tail -1")
 
-# Checks the exit status of the brew doctor to command to ensure it exited 0, indicating a successful install
-if [[ $? -eq 0 ]]; then
-	echo "Homebrew installation successful..."
+# Add Homebrew's "bin" to target user PATH
+if [ -n "${get_path_cmd}" ]; then
+	/usr/bin/su - "${mostCommonUser}" -c "${get_path_cmd}"
+fi
+
+# Check Homebrew install status, check with the doctor status to see if everything looks good
+if /usr/bin/su - "${mostCommonUser}" -i -c "${brew_bin} doctor"; then
+	echo 'Homebrew Installation Complete! Your system is ready to brew.'
 	exit 0
 else
-	echo "Homebrew installation failed..."
+	echo 'Homebrew Installation Failed'
 	exit 1
 fi
