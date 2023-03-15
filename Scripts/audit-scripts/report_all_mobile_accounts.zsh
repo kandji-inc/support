4f2bc1622ba2f43461dc7dc73cf1de1773af3c31
nick-mcdonald-dtnb <nick.mcdonald@kandji.io>
2023-03-15 23:38:59 +0000

    Automated Push

--- Scripts/audit-scripts/report_all_mobile_accounts.zsh
+++ Scripts/audit-scripts/report_all_mobile_accounts.zsh
@@ -1,17 +1,55 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
+################################################################################################
+# Created by Matt Wilson | support@kandji.io | Kandji, Inc.
+################################################################################################
+#
+#   Updated - 2023-01-27 - Matt Wilson
+#
+################################################################################################
+# TESTED MACOS VERSIONS
+################################################################################################
+#
+#   - 13.2
+#
+################################################################################################
+# SOFTWARE INFORMATION
+################################################################################################
 #
 #   Get all user accounts with a UID greater than 500 and report if they are mobile accounts
 #
+################################################################################################
+# License Information
+################################################################################################
+#
+# Copyright 2023 Kandji, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+# to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+################################################################################################
 
 # Creates a list of users with a UID greater than 500
 # Users with a UID less than UID 500 are typically services accuonts
-USER_LIST=($(/usr/bin/dscl . list /Users UniqueID |
+USER_LIST=("$(/usr/bin/dscl . list /Users UniqueID |
     /usr/bin/awk '$2 > 500 {print $1}' |
-    /usr/bin/sed -e 's/^[ \t]*//'))
+    /usr/bin/sed -e 's/^[ \t]*//')")
 
 # Check to see if the user_list is empty
-if [[ "$USER_LIST" == "" ]]; then
+if [[ "${#USER_LIST[@]}" -eq 0 ]]; then
     # If no users with UID over 1000 are returned, Quit.
     echo "No user accounts found."
     echo "Nothing to do ..."
@@ -21,7 +59,7 @@ if [[ "$USER_LIST" == "" ]]; then
 fi
 
 # If the list contianed users with UID over 1000 print them to stdout
-for user in $USER_LIST; do
+for user in "${USER_LIST[@]}"; do
 
     # Verify that the accounts found are actually mobile accounts
     echo "Checking user account type for $user ..."
--- Scripts/audit-scripts/report_all_standard_users.zsh
+++ Scripts/audit-scripts/report_all_standard_users.zsh
@@ -1,18 +1,56 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
+################################################################################################
+# Created by Matt Wilson | support@kandji.io | Kandji, Inc.
+################################################################################################
+#
+#   Updated - 2023-01-27 - Matt Wilson
+#
+################################################################################################
+# TESTED MACOS VERSIONS
+################################################################################################
+#
+#   - 13.2
+#
+################################################################################################
+# SOFTWARE INFORMATION
+################################################################################################
 #
 #   Report all local user accounts with a UID greater than 500
 #
+################################################################################################
+# License Information
+################################################################################################
+#
+# Copyright 2023 Kandji, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+# to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+################################################################################################
 
 # Creates a list of users with a UID greater than 500
 # Users with a UID less than UID 500 are typically services accuonts
-USER_LIST=($(/usr/bin/dscl . list /Users UniqueID |
-    /usr/bin/awk '$2 > 500 {print $1}'))
+USER_LIST=("$(/usr/bin/dscl . list /Users UniqueID |
+    /usr/bin/awk '$2 > 500 {print $1}')")
 
 echo "Checking user account permissions ..."
 
 # Determine if any of the local users have standard permissions
-for user in $USER_LIST; do
+for user in "${USER_LIST[@]}"; do
 
     # Verify that the accounts found are actually mobile accounts
     # Returns true if the current logged in user is a member of the local admins group.
