07f7338636fbaa40d2cea742106323a4cb37eb91
nick-mcdonald-dtnb <nick.mcdonald@kandji.io>
2023-03-24 19:37:03 +0000

    Automated Push

---
+++ api-tools/installed-apps/README.md
@@ -0,0 +1,105 @@
+# App Installation Report
+
+**NOTE**: As with any script please be sure to test with a subset of devices.
+
+### About
+
+This `python3` script leverages the Kandji API to generate a `CSV` report containing devices with a specific app installed or all devices with all installed apps.
+
+### Kandji API
+
+- The API permissions required to run the reporting script are as follows. Checkout the Kandji [Knowledge Base](https://support.kandji.io) for more information.
+
+    <img src="images/api_permissions_required.png" alt="drawing" width="1024"/>
+
+### Dependencies
+
+- This script relies on Python 3 to run. Python 3 can be installed directly as an [Auto App](https://updates.kandji.io/auto-app-python-3-214020), from [python.org](https://www.python.org/downloads/), or via [homebrew](https://brew.sh)
+
+- Python dependencies can be installed individually below, or with the included `requirements.txt` file using the following command from a Terminal: `python3 -m pip install -r requirements.txt`
+
+    ```
+    python3 -m pip install requests
+    python3 -m pip install pathlib
+    ```
+
+### Script Modification
+
+1. Open the script in a text editor such as BBEdit or VSCode.
+1. Update the `SUBDOMAIN` variable to match your Kandji subdomain, the Kandji tenant `REGION`, and update `TOKEN` information with your Bearer token.
+
+    - The `BASE_URL`, `REGION`, and `TOKEN` can be found by logging into Kandji then navigate to `Settings > Access > API Token`. From there, you can copy the information out of the API URL and generate API tokens.
+    - For US-based tenants the `REGION` can either be `us` or left as an empty string (`""`)
+
+        *NOTE*: The API token is only visible at the point of creation so be sure to copy it to a safe location.
+
+        ```python
+        ########################################################################################
+        ######################### UPDATE VARIABLES BELOW #######################################
+        ########################################################################################
+
+        SUBDOMAIN = "accuhive"  # bravewaffles, example, company_name
+
+        # us("") and eu - this can be found in the Kandji settings on the Access tab
+        REGION = ""
+
+        # Kandji Bearer Token
+        TOKEN = ""
+        ```
+
+1. Save and close the script.
+
+### Running the Script
+
+1. Copy this script to a common location. i.e. Desktop
+2. Launch a Terminal window and navigate to your Desktop using the following command.
+
+    `cd ~/Desktop`
+
+3. Enter the following command in the Terminal window to run the script.
+
+    `python3 installed_apps.py --name "Atom"`  
+
+    **Example output**
+
+    ```sh
+    Version: 1.3.1
+
+    Base URL: https://accuhive.api.kandji.io/api
+
+    Looking for devices with "Atom" installed...
+    Getting device inventory from Kandji...
+    Total records returned: 54
+    Looking for installed apps...
+    Found 7 devices with "Atom" installed...
+    Generating Kandji app install report ...
+    Kandji app report complete ...
+    Kandji app report at: /Users/example/installed-apps/atom_app_install_report_20230307.csv
+    ```
+
+4. If the `installed_apps.py` script is executed, a file with the name `hyper_installed_apps_<YYYYMMDD>.csv` will be generated in the current directory, which, in this case would the `Desktop`.
+
+### Examples
+
+- To see `--help` information, use
+
+    ```shell
+    python installed_apps.py --help
+    
+    usage: installed_apps [-h] [--name "Atom"] [--version]
+
+    Generates a report containing devices with a specific app installed or a report containing all installed apps.
+
+    options:
+    -h, --help     show this help message and exit
+    --name "Atom"  Enter a specific app name. This will limit the search results to only the specified app. Example: --name "Kandji Self Service"
+    --version      Show this tool's version.
+    ```
+
+- To return all apps installed on all devices, use.
+
+    `python3 installed_apps.py`
+
+- To return all devices with "Kandji Self Service" installed, use.
+
+    `python3 installed_apps.py --name "Kandji Self Service`
--- api-tools/installed-apps/installed_apps.py
+++ api-tools/installed-apps/installed_apps.py
@@ -1,52 +1,48 @@
 #!/usr/bin/env python3
 
-"""
-Create a report of installed apps.
+"""Create a report of installed apps."""
 
-Return a report of all apps, app versions, number of installations, device name, and
-serial numbers where the, app is installed from a specified Kandji instance.
-"""
-
-########################################################################################
+################################################################################################
 # Created by Matt Wilson | support@kandji.io | Kandji, Inc.
-########################################################################################
+################################################################################################
 #
 # Created:  2021.06.03
-# Modified: 2022.09.09
+# Modified: 2023.02.07
 #
-########################################################################################
+################################################################################################
 # Software Information
-########################################################################################
+################################################################################################
 #
 # This python3 script leverages the Kandji API to generate a CSV report containing a
 # list of every installed application recorded by the Kandji Web App. The information
 # includes application name, the application version, the device name, and the device
 # serial numbers.
 #
-########################################################################################
+################################################################################################
 # License Information
-########################################################################################
-# Copyright 2022 Kandji, Inc.
+################################################################################################
+#
+# Copyright 2023 Kandji, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this
 # software and associated documentation files (the "Software"), to deal in the Software
-# without restriction, including without limitation the rights to use, copy, modify,
-# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to the following
-# conditions:
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+# to whom the Software is furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all copies
-# or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CNNECTION WITH THE SOFTWARE
-# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-########################################################################################
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+################################################################################################
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 
 # Standard library
@@ -74,21 +70,26 @@ from requests.adapters import HTTPAdapter
 ########################################################################################
 
 SUBDOMAIN = "accuhive"  # bravewaffles, example, company_name
-REGION = "us"  # us and eu - this can be found in the Kandji settings on the Access tab
+
+# us("") and eu - this can be found in the Kandji settings on the Access tab
+REGION = ""  # us and eu - this can be found in the Kandji settings on the Access tab
 
 # Kandji Bearer Token
-TOKEN = "your_api_key_here"
+TOKEN = ""
 
 ########################################################################################
 ######################### DO NOT MODIFY BELOW THIS LINE ################################
 ########################################################################################
 
-# Initialize some variables
 # Kandji API base URL
-if REGION == "us":
-    BASE_URL = f"https://{SUBDOMAIN}.clients.{REGION}-1.kandji.io/api"
+if REGION in ["", "us"]:
+    BASE_URL = f"https://{SUBDOMAIN}.api.kandji.io/api"
+
+elif REGION in ["eu"]:
+    BASE_URL = f"https://{SUBDOMAIN}.api.{REGION}.kandji.io/api"
+
 else:
-    BASE_URL = f"https://{SUBDOMAIN}.clients.{REGION}.kandji.io/api"
+    sys.exit(f'\nUnsupported region "{REGION}". Please update and try again\n')
 
 TODAY = datetime.today().strftime("%Y%m%d")
 
@@ -105,24 +106,24 @@ HERE = pathlib.Path("__file__").parent
 
 def var_validation():
     """Validate variables."""
-    if "accuhive" in BASE_URL:
+    if SUBDOMAIN in ["", "accuhive"]:
         print(
-            f'\n\tThe subdomain "{SUBDOMAIN}" in {BASE_URL} needs to be updated to '
+            f'\nThe subdomain "{SUBDOMAIN}" in {BASE_URL} needs to be updated to '
             "your Kandji tenant subdomain..."
         )
-        print("\tPlease see the example in the README for this repo.\n")
+        print("Please see the example in the README for this repo.\n")
         sys.exit()
 
-    if "api_key" in TOKEN:
-        print(f'\n\tThe TOKEN should not be "{TOKEN}"...')
-        print("\tPlease update this to your API Token.\n")
+    if TOKEN in ["api_key", ""]:
+        print(f'\nThe TOKEN should not be "{TOKEN}"...')
+        print("Please update this to your API Token.\n")
         sys.exit()
 
 
 def program_arguments():
     """Return arguments."""
     parser = argparse.ArgumentParser(
-        prog="app_install_report",
+        prog="installed_apps",
         description=(
             "Generates a report containing devices with a specific app installed or a "
             "report containing all installed apps."
@@ -146,7 +147,7 @@ def program_arguments():
     return parser.parse_args()
 
 
-def error_handling(resp, resp_code, err_msg):
+def http_errors(resp, resp_code, err_msg):
     """Handle HTTP errors."""
     # 400
     if resp_code == requests.codes["bad_request"]:
@@ -167,12 +168,12 @@ def error_handling(resp, resp_code, err_msg):
     # 404
     elif resp_code == requests.codes["not_found"]:
         print("\nWe cannot find the one that you are looking for...")
-        print("Move along...\n")
+        print("Move along...")
         print(f"\tError: {err_msg}")
         print(f"\tResponse msg: {resp}")
         print(
-            "\tPossible reason: If this is a device it could be because the device is "
-            "not longer\n"
+            "\tPossible reason: If this is a device, it could be because the device is "
+            "no longer\n"
             "\t\t\t enrolled in Kandji. This would prevent the MDM command from being\n"
             "\t\t\t sent successfully.\n"
         )
@@ -191,7 +192,7 @@ def error_handling(resp, resp_code, err_msg):
     else:
         print("Something really bad must have happened...")
         print(err_msg)
-        # sys.exit()
+        sys.exit()
 
 
 def kandji_api(method, endpoint, params=None, payload=None):
@@ -225,11 +226,11 @@ def kandji_api(method, endpoint, params=None, payload=None):
             except Exception:
                 data = response.text
 
-        # if the request is successful exeptions will not be raised
+        # if the request is successful exceptions will not be raised
         response.raise_for_status()
 
     except requests.exceptions.RequestException as err:
-        error_handling(resp=response, resp_code=response.status_code, err_msg=err)
+        http_errors(resp=response, resp_code=response.status_code, err_msg=err)
         data = {"error": f"{response.status_code}", "api resp": f"{err}"}
 
     return data
@@ -250,9 +251,8 @@ def get_devices(params=None, ordering="serial_number"):
         params.update(
             {"ordering": f"{ordering}", "limit": f"{limit}", "offset": f"{offset}"}
         )
-        # print(params)
 
-        # check to see if a platform was sprecified
+        # check to see if a platform was specified
         response = kandji_api(method="GET", endpoint="/v1/devices", params=params)
 
         count += len(response)
@@ -359,21 +359,13 @@ def write_report(report_payload, report_name):
 
 def main():
     """Run main logic."""
-    # validate vars
-    var_validation()
     # Return the arguments
     arguments = program_arguments()
 
+    var_validation()
+
     print(f"Version: {__version__}\n")
     print(f"Base URL: {BASE_URL}\n")
-
-    if "example" in BASE_URL:
-        print(f"\tThe subdomain in {BASE_URL} needs to be updated...\n")
-        sys.exit()
-
-    if "your_api_key_here" in TOKEN:
-        print(f'\tThe TOKEN should not be "{TOKEN}"...\n')
-        sys.exit()
 
     # dict placeholder for params passed to api requests
     params_dict = {}
